d9b3a4f0800755c15c8c718817c6200e114732c0
Josefina Albornoz <josefinaalbornoz26@gmail.com>
2024-02-26 13:21:15 -0300

    visual pantalla principal

--- gestor-cultural/inicio.py
+++ gestor-cultural/inicio.py
@@ -1,42 +1,47 @@
 import tkinter as tk
+from tkinter import *
 from tkinter import messagebox
-
-#Falta migrar la parte visual
+from pantalla_principal import PantallaPrincipal
 
 class Login:
     def __init__(self, master):
         self.master = master
-        self.master.title("Login")
-        self.master.geometry("300x150")
+        self.master.title("Iniciar sesión")
+        self.master.geometry("900x500")
+        self.master.configure(bg='purple')
 
-        self.label_user = tk.Label(self.master, text="Usuario:")
-        self.label_user.pack()
-        self.entry_user = tk.Entry(self.master)
-        self.entry_user.pack()
+        self.frame = Frame(self.master, bg='purple')
 
-        self.label_password = tk.Label(self.master, text="Contraseña:")
-        self.label_password.pack()
-        self.entry_password = tk.Entry(self.master, show="*")
-        self.entry_password.pack()
+        self.inicio_label = Label(self.frame, text="Iniciar sesión", bg='purple', fg="white", font=("Arial", 40))
+        self.sub_label = Label(self.frame, text="Gestion cultural - Jujuy", bg='purple', fg="white", font=("Arial", 25))
+        self.usuario_label = Label(self.frame, text="Usuario", bg='purple', fg="#FFFFFF", font=("Arial", 16))
+        self.usuario_entry = Entry(self.frame, font=("Arial", 16))
+        self.contraseña_entry = Entry(self.frame, show="*", font=("Arial", 16))
+        self.contraseña_label = Label(self.frame, text="Contraseña", bg='purple', fg="#FFFFFF", font=("Arial", 16))
+        self.boton_login = Button(self.frame, text="Ingresar", bg="#FF3399", fg="#FFFFFF", font=("Arial", 16), command=self.login)
 
-        self.btn_login = tk.Button(self.master, text="Iniciar sesión", command=self.login)
-        self.btn_login.pack()
+        self.inicio_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
+        self.sub_label.grid(row=1, column=0, columnspan=2, sticky="news", pady=40)
+        self.usuario_label.grid(row=2, column=0, padx=5)
+        self.usuario_entry.grid(row=2, column=1, pady=10)
+        self.contraseña_label.grid(row=3, column=0, padx=5)
+        self.contraseña_entry.grid(row=3, column=1, pady=10)
+        self.boton_login.grid(row=4, column=0, columnspan=2, pady=30)
+
+        self.frame.pack()
 
     def login(self):
-        # Validar usuario y contraseña
         usuario_correcto = "admin"
         contraseña_correcta = "1234"
 
-        usuario = self.entry_user.get()
-        contraseña = self.entry_password.get()
+        usuario = self.usuario_entry.get()
+        contraseña = self.contraseña_entry.get()
 
         if usuario == usuario_correcto and contraseña == contraseña_correcta:
             self.master.destroy()
-            from pantalla_principal import PantallaPrincipal
             PantallaPrincipal()
         else:
             messagebox.showerror("Error", "Usuario o contraseña incorrectos")
-
 
 if __name__ == "__main__":
     root = tk.Tk()
