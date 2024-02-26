06ffa963e3a184d52c187ca5ab8f874a3c286948
Josefina Albornoz <josefinaalbornoz26@gmail.com>
2024-02-26 18:21:25 -0300

    listas
    se agregó una visualizacion de mis listas

--- gestor-cultural/client/listas.py
+++ gestor-cultural/client/listas.py
@@ -1,4 +1,126 @@
-#Idealmente, este archivo contendría la ventana
-#la cual me carga las listas de las tablas existentes en la base de datos
-#No la llegué a realizar, porque ya tengo algunas listas y busquedas en los editores y esperaba tener todas las tablas para este punto
-#Pero puedo intentar mostrar las lustas que ya funcionan (artistas - lugares)
+import tkinter as tk
+from tkinter import ttk
+import sqlite3
+
+#Visualizador de mis listas
+
+class VisualizacionTablas(tk.Tk):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.configure(bg='purple')
+        self.title("Visualización de Tablas")
+
+        self.conexion_db = sqlite3.connect('database/gestion_cultural.db')
+        self.cursor = self.conexion_db.cursor()
+
+        self.frame_botones = ttk.Frame(self)
+        self.frame_botones.pack()
+
+        self.btn_artistas = ttk.Button(self.frame_botones, text="Artistas", command=self.mostrar_artistas)
+        self.btn_artistas.grid(row=0, column=0, padx=5, pady=5)
+
+        self.btn_eventos = ttk.Button(self.frame_botones, text="Eventos", command=self.mostrar_eventos)
+        self.btn_eventos.grid(row=0, column=1, padx=5, pady=5)
+
+        self.btn_lugares = ttk.Button(self.frame_botones, text="Lugares", command=self.mostrar_lugares)
+        self.btn_lugares.grid(row=0, column=2, padx=5, pady=5)
+
+        self.btn_artistas_eventos = ttk.Button(self.frame_botones, text="Artistas_Eventos", command=self.mostrar_artistas_eventos)
+        self.btn_artistas_eventos.grid(row=0, column=3, padx=5, pady=5)
+
+        self.btn_eventos_lugares = ttk.Button(self.frame_botones, text="Eventos_Lugares", command=self.mostrar_eventos_lugares)
+        self.btn_eventos_lugares.grid(row=0, column=4, padx=5, pady=5)
+
+        self.frame_tablas = ttk.Frame(self)
+        self.frame_tablas.pack()
+
+        self.contenidos_artistas = None
+        self.contenidos_eventos = None
+        self.contenidos_lugares = None
+        self.contenidos_artistas_eventos = None
+        self.contenidos_eventos_lugares = None
+
+    def obtener_contenido_tabla(self, nombre_tabla):
+        self.cursor.execute(f"SELECT * FROM {nombre_tabla};")
+        return self.cursor.fetchall()
+
+    def mostrar_artistas(self):
+        if self.contenidos_artistas is None:
+            self.contenidos_artistas = self.obtener_contenido_tabla('artistas')
+            self.mostrar_contenidos('Artistas', self.contenidos_artistas)
+        else:
+            self.mostrar_contenidos('Artistas', self.contenidos_artistas)
+
+    def mostrar_eventos(self):
+        if self.contenidos_eventos is None:
+            self.contenidos_eventos = self.obtener_contenido_tabla('eventos')
+            self.mostrar_contenidos('Eventos', self.contenidos_eventos)
+        else:
+            self.mostrar_contenidos('Eventos', self.contenidos_eventos)
+
+    def mostrar_lugares(self):
+        if self.contenidos_lugares is None:
+            self.contenidos_lugares = self.obtener_contenido_tabla('lugares')
+            self.mostrar_contenidos('Lugares', self.contenidos_lugares)
+        else:
+            self.mostrar_contenidos('Lugares', self.contenidos_lugares)
+
+    def mostrar_artistas_eventos(self):
+        if self.contenidos_artistas_eventos is None:
+            self.contenidos_artistas_eventos = self.obtener_contenido_artistas_eventos()
+            self.mostrar_contenidos('Artistas_Eventos', self.contenidos_artistas_eventos)
+        else:
+            self.mostrar_contenidos('Artistas_Eventos', self.contenidos_artistas_eventos)
+
+    def mostrar_eventos_lugares(self):
+        if self.contenidos_eventos_lugares is None:
+            self.contenidos_eventos_lugares = self.obtener_contenido_eventos_lugares()
+            self.mostrar_contenidos('Eventos_Lugares', self.contenidos_eventos_lugares)
+        else:
+            self.mostrar_contenidos('Eventos_Lugares', self.contenidos_eventos_lugares)
+
+    def obtener_contenido_artistas_eventos(self):
+        self.cursor.execute("SELECT artistas.nombre, eventos.nombre FROM artistas_eventos "
+                            "INNER JOIN artistas ON artistas_eventos.id_artista = artistas.id_artista "
+                            "INNER JOIN eventos ON artistas_eventos.id_evento = eventos.id_evento;")
+        return self.cursor.fetchall()
+
+    def obtener_contenido_eventos_lugares(self):
+        self.cursor.execute("SELECT eventos.nombre, lugares.nombre FROM eventos_lugares "
+                            "INNER JOIN eventos ON eventos_lugares.id_evento = eventos.id_evento "
+                            "INNER JOIN lugares ON eventos_lugares.id_lugar = lugares.id_lugar;")
+        return self.cursor.fetchall()
+
+    def mostrar_contenidos(self, nombre_tabla, contenido_tabla):
+        for widget in self.frame_tablas.winfo_children():
+            widget.destroy()
+
+        tk.Label(self.frame_tablas, text="Tabla").pack()
+        tabla = ttk.Treeview(self.frame_tablas, show='headings')
+
+        if nombre_tabla == 'Artistas_Eventos':
+            tabla["columns"] = ("#0", "#1", "#2")
+            tabla.heading("#0", text="ID")
+            tabla.heading("#1", text="Nombre Artista")
+            tabla.heading("#2", text="Nombre Evento")
+        elif nombre_tabla == 'Eventos_Lugares':
+            tabla["columns"] = ("#0", "#1", "#2")
+            tabla.heading("#0", text="ID")
+            tabla.heading("#1", text="Nombre Evento")
+            tabla.heading("#2", text="Nombre Lugar")
+        else:
+            # Obtener los nombres de las columnas
+            column_names = [description[0] for description in self.cursor.description]
+            tabla["columns"] = tuple(column_names)
+            for i, columna in enumerate(column_names):
+                tabla.heading(columna, text=columna)
+
+        for fila in contenido_tabla:
+            tabla.insert('', 'end', values=fila)
+        tabla.pack()
+
+
+if __name__ == "__main__":
+    app = VisualizacionTablas(None)
+    app.mainloop()
--- gestor-cultural/pantalla_principal.py
+++ gestor-cultural/pantalla_principal.py
@@ -22,13 +22,13 @@ class PantallaPrincipal(tk.Tk):
         texto1 = tk.Label(frame, text='Ingrese a las listas', font=('Arial', 12), fg='white', bg='purple')
         texto2 = tk.Label(frame, text='Ingrese a los editores', font=('Arial', 12), fg='white', bg='purple')
 
-        buscar_boton = Button(frame, text='Listas', width=20, height=2, font=('Arial', 15), bg='orange', fg='white', cursor='hand2')
+        listas_boton = Button(frame, text='Listas', width=20, height=2, font=('Arial', 15), bg='orange', fg='white', cursor='hand2', command=self.abrir_listas)
         artistas_boton = Button(frame, text='Artistas', width=20, height=2, font=('Arial', 15), bg='blue', fg='white', cursor='hand2', command=self.abrir_editor_artista)
         lugares_boton = Button(frame, text='Lugares', width=20, height=2, font=('Arial', 15), bg='green', fg='white', cursor='hand2', command=self.abrir_editor_lugar)
         eventos_boton = Button(frame, text='Eventos', width=20, height=2, font=('Arial', 15), bg='red', fg='white', cursor='hand2', command=self.abrir_editor_evento)
 
         texto1.grid(row=0, column=1, padx=10, pady=(20))
-        buscar_boton.grid(row=1, column=1, padx=10, pady=10, sticky="e")
+        listas_boton.grid(row=1, column=1, padx=10, pady=10, sticky="e")
         texto2.grid(row=2, column=1, padx=10, pady=(20))
         artistas_boton.grid(row=3, column=1, padx=10, pady=(20), sticky="e")
         lugares_boton.grid(row=4, column=1, padx=10, pady=10, sticky="e")
@@ -36,6 +36,13 @@ class PantallaPrincipal(tk.Tk):
 
     #Tkinter tiene problemas para abrir distintas ventanas, por lo que al trabajar desde mi compu
     # para poder visualizarlas preferí llamar a la ruta exacta de mis archivos
+        
+    def abrir_listas(self):
+        try:
+            os.system("python C:/Users/josef/proyectos/proyecto-integrador/gestor-cultural/client/listas.py")
+            print("Script listas.py se está ejecutando")
+        except Exception as e:
+            print("Error al abrir editor_artista.py:", e)
 
     def abrir_editor_artista(self):
         try:
